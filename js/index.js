d01a874873f262a16064756dc96357d6f8eb8786
Rahul T <rahulmanojcoder@gmail.com>
2022-01-22 13:16:35 +0530

    hero partial

--- js/index.js
+++ js/index.js
@@ -12,3 +12,26 @@ function fixNav() {
         nav.classList.remove('active')
     }
 }
+
+
+// SLIDER
+
+const btns = document.querySelectorAll(".nav-btn")
+const slides = document.querySelectorAll(".image-slide")
+
+let sliderNav = (index) => {
+    btns.forEach(btn => {
+        btn.classList.remove("active")
+    })
+
+    slides.forEach(slide => {
+        slide.classList.remove("active")
+    })
+    btns[index].classList.add("active")
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i)
+    })
+})
